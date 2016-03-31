cbf5d472198ed6e2bbafd11da14bf889bfc20699
Fabian Baier <fabian.baier@gmx.de>
2016-03-31 17:32:20 +0200

    first try of visualservoing

--- include/masterslave/manipulation/VisualServoing.h
+++ include/masterslave/manipulation/VisualServoing.h
@@ -13,20 +13,42 @@
 #include <eigen_conversions/eigen_msg.h>
 #include <tf/transform_listener.h>
 
+#include <dynamic_reconfigure/server.h>
+#include "masterslave/VisualServoingConfig.h"
+
 class VisualServoing {
 public:
     VisualServoing(ros::NodeHandle &nh);
 private:
     void markerCallback(const ar_track_alvar_msgs::AlvarMarkersConstPtr& markerPtr);
+    void configurationCallback(masterslave::VisualServoingConfig& config, uint32_t level);
     bool visualServoingCallback(masterslave::Manipulation::Request& req, masterslave::Manipulation::Response& resp);
+    Eigen::Vector3d calculateTranslationalPID();
+    Eigen::Matrix3d calculateRotationalPID();
     ros::NodeHandle nh_;
     ros::Subscriber markerSub;
     ros::ServiceServer visualServoingServer;
     Eigen::Affine3d initialTransform;
     Eigen::Affine3d actualTransform;
     Eigen::Affine3d differenceTransform;
+    Eigen::Affine3d differenceTransformOld;
     std::array<int,2> ids = {{0,1}};
     bool initialRun = true;
+
+    double cycleTime{0.033};
+
+    double pTrans{1};
+    double dTrans{0.001};
+    double iTrans{0.0001};
+
+    Eigen::Vector3d integralErrorTrans{0,0,0};
+
+    double pRot{1};
+    double dRot{0};
+    double iRot{0};
+
+    Eigen::Vector3d integralErrorRot;
+
 };
 
 #endif // VISUALSERVOING_H
--- src/manipulation/VisualServoing.cpp
+++ src/manipulation/VisualServoing.cpp
@@ -2,13 +2,47 @@
 
 VisualServoing::VisualServoing(ros::NodeHandle &nh): nh_(nh)
 {
+    differenceTransform = Eigen::Affine3d::Identity();
+    differenceTransformOld = Eigen::Affine3d::Identity();
+    dynamic_reconfigure::Server<masterslave::VisualServoingConfig> server;
+    dynamic_reconfigure::Server<masterslave::VisualServoingConfig>::CallbackType f;
+    f = boost::bind(&VisualServoing::configurationCallback,this ,_1,_2);
+    server.setCallback(f);
     markerSub = nh_.subscribe("/ar_pose_marker",1,&VisualServoing::markerCallback,this);
-    //visualServoingServer = nh_.advertiseService("/Manipulation",&VisualServoing::visualServoingCallback,this);
+    visualServoingServer = nh_.advertiseService("/Manipulation",&VisualServoing::visualServoingCallback,this);
     ros::spin();
+}
+
+Eigen::Vector3d VisualServoing::calculateTranslationalPID()
+{
+    Eigen::Vector3d retValue = Eigen::Vector3d::Zero();
+    integralErrorTrans = iTrans*differenceTransform.translation()*cycleTime;
+    retValue = pTrans*differenceTransform.translation() + dTrans*(differenceTransform.translation()-differenceTransformOld.translation())/cycleTime + integralErrorTrans;
+    ROS_INFO_STREAM(retValue);
+    return retValue;
+}
+
+Eigen::Matrix3d VisualServoing::calculateRotationalPID()
+{
+    Eigen::Quaterniond temp;
+    Eigen::Quaterniond acutalRotation(actualTransform.rotation());
+    Eigen::Quaterniond inititialRotaion(initialTransform.rotation());
+    temp = acutalRotation.slerp(pRot*cycleTime,inititialRotaion);
+    ROS_INFO_STREAM(temp.toRotationMatrix());
+    return temp.toRotationMatrix();
 }
 
 bool VisualServoing::visualServoingCallback(masterslave::Manipulation::Request &req, masterslave::Manipulation::Response &resp)
 {
+    Eigen::Affine3d T_0_EE_old;
+    Eigen::Affine3d T_0_EE_new = Eigen::Affine3d::Identity();
+    tf::poseMsgToEigen(req.T_0_EE_old,T_0_EE_old);
+    T_0_EE_new.translate(T_0_EE_old.translation()-0.1*calculateTranslationalPID());
+    T_0_EE_new.rotate(T_0_EE_old.rotation());
+    ROS_WARN_STREAM(T_0_EE_old.matrix());
+    ROS_WARN_STREAM(T_0_EE_new.matrix());
+    tf::poseEigenToMsg(T_0_EE_new,resp.T_0_EE_new);
+    return true;
 
 }
 
@@ -17,6 +51,8 @@ void VisualServoing::markerCallback(const ar_track_alvar_msgs::AlvarMarkersConst
     int markerFound = 0;
     Eigen::Affine3d tcp;
     Eigen::Affine3d obj;
+    differenceTransformOld = differenceTransform;
+    differenceTransform = Eigen::Affine3d::Identity();
     for(int i=0; i< markerPtr->markers.size(); i++)
     {
         switch(markerPtr->markers[i].id)
@@ -39,9 +75,21 @@ void VisualServoing::markerCallback(const ar_track_alvar_msgs::AlvarMarkersConst
             initialTransform = actualTransform;
             initialRun = false;
         }
+        differenceTransform = actualTransform*initialTransform.inverse();
+        ROS_WARN_STREAM(differenceTransform.matrix());
     }
-    differenceTransform = actualTransform*initialTransform.inverse();
-    ROS_INFO_STREAM("actual Transform" << actualTransform.matrix() << "\n difference Transform" << differenceTransform.matrix());
+}
+
+void VisualServoing::configurationCallback(masterslave::VisualServoingConfig &config, uint32_t level)
+{
+    pRot = config.PRot;
+    dRot = config.DRot;
+    iRot = config.IRot;
+
+    pTrans = config.PTrans;
+    dTrans = config.DTrans;
+    iTrans = config.ITrans;
+
 }
 
 int main(int argc, char** argv)
